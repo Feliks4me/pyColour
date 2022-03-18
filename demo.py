e48e567621f4f825c9d9b79087fd6d67e9c60fbc
Feliks <66078338+Feliks4me@users.noreply.github.com>
2022-03-18 15:25:25 -0400

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,10 @@
+#import the library
+import color
+
+#print in color
+color.prt("red", "debug")
+
+#input in color
+demo = color.inp("blue", "Enter your name, ")
+
+color.prt("red", f"your name is {demo}")
